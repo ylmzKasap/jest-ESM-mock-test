83c9c2524c01fa2143ed1facf5f336ff5723224b
Yılmaz Kasap <ylmzkasap@outlook.com>
2023-11-21 22:26:38 +0300

    Add more comments

--- message_controller.test.js
+++ message_controller.test.js
@@ -4,13 +4,19 @@ import { jest } from '@jest/globals';
 
 const server = request(app());
 
+/* 
+Doesn't work with this either, but is it even necessary in this case?
+
+const mockModule = await import('./module_to_mock.js'); 
+*/
+
 // This is supposed to be mocking 'moduleToMock' imported in './message_controller.js'
 jest.unstable_mockModule('./module_to_mock.js', () => 
   jest.fn().mockReturnValue(true) 
 );
 
 test('Mock successfully', async () => {
-  const messageToSend = '';
+  const messageToSend = 'hello';
   const response = await server.post('/message').send(
     {message: messageToSend}
   );
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Mock indirect ES modules with Jest
+
+The test case should pass after running 'npm run test' given that mock works.
