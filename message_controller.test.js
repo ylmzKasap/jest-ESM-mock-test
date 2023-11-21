d53115318673e1b03d68bcca1a4336483fb709b3
Yılmaz Kasap <ylmzkasap@outlook.com>
2023-11-21 21:56:15 +0300

    Add comments

--- message_controller.test.js
+++ message_controller.test.js
@@ -4,16 +4,16 @@ import { jest } from '@jest/globals';
 
 const server = request(app());
 
+// This is supposed to be mocking 'moduleToMock' imported in './message_controller.js'
 jest.unstable_mockModule('./module_to_mock.js', () => 
   jest.fn().mockReturnValue(true) 
 );
 
-test('Send successfully', async () => {
-  const messageToSend = 'hi mom';
-  const response = await server.post('/message')
-    .send({
-      message: messageToSend
-    });
+test('Mock successfully', async () => {
+  const messageToSend = '';
+  const response = await server.post('/message').send(
+    {message: messageToSend}
+  );
 
   expect(response.body.message).toEqual(`Mock works: ${messageToSend}`);
 });
